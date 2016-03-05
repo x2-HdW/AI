78d5e1417a2c005331fa04af801c3b2270c81735
x2-HdW <schmidket@googlemail.com>
2016-03-05 23:37:35 +0100

    prepared test to check coverage status change

--- modules/example/src/main.cpp
+++ modules/example/src/main.cpp
@@ -5,5 +5,39 @@ using namespace std;
 int main(int argc, char** argv)
 {
 	cout << "Hello world!" << endl;
+	cout << "Hello world!" << endl;
+	cout << "Hello world!" << endl;
+	cout << "Hello world!" << endl;
+	cout << "Hello world!" << endl;
+	
+	cout << "Hello world!" << endl;
+	cout << "Hello world!" << endl;
+	cout << "Hello world!" << endl;
+	cout << "Hello world!" << endl;
+	cout << "Hello world!" << endl;
+	
+	cout << "Hello world!" << endl;
+	cout << "Hello world!" << endl;
+	cout << "Hello world!" << endl;
+	cout << "Hello world!" << endl;
+	cout << "Hello world!" << endl;
+	
+	cout << "Hello world!" << endl;
+	cout << "Hello world!" << endl;
+	cout << "Hello world!" << endl;
+	cout << "Hello world!" << endl;
+	cout << "Hello world!" << endl;
+	
+	cout << "Hello world!" << endl;
+	cout << "Hello world!" << endl;
+	cout << "Hello world!" << endl;
+	cout << "Hello world!" << endl;
+	cout << "Hello world!" << endl;
+	
+	if(false)
+	{
+		cout << "Hello world!" << endl;
+		cout << "Hello world!" << endl;
+	}
 	return 0;
 }
