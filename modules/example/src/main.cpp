db80db9dc41e003911253180ced16d05dad491e3
x2-HdW <schmidket@googlemail.com>
2016-03-06 10:56:34 +0100

    yet another

--- modules/example/src/main.cpp
+++ modules/example/src/main.cpp
@@ -10,33 +10,10 @@ int main(int argc, char** argv)
 	cout << "Hello world!" << endl;
 	cout << "Hello world!" << endl;
 	
-	/*cout << "Hello world!" << endl;
 	cout << "Hello world!" << endl;
-	cout << "Hello world!" << endl;
-	cout << "Hello world!" << endl;
-	cout << "Hello world!" << endl;
-	
-	cout << "Hello world!" << endl;
-	cout << "Hello world!" << endl;
-	cout << "Hello world!" << endl;
-	cout << "Hello world!" << endl;
-	cout << "Hello world!" << endl;
-	
-	cout << "Hello world!" << endl;
-	cout << "Hello world!" << endl;
-	cout << "Hello world!" << endl;
-	cout << "Hello world!" << endl;
-	cout << "Hello world!" << endl;
-	
-	cout << "Hello world!" << endl;
-	cout << "Hello world!" << endl;
-	cout << "Hello world!" << endl;
-	cout << "Hello world!" << endl;
-	cout << "Hello world!" << endl;*/
 	
 	if(argc > 15)
 	{
-		cout << "Hello world!" << endl;
 		cout << "Hello world!" << endl;
 	}
 	return 0;
